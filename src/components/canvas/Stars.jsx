7250e6f7242a6cd4490fd04716b5fc635603d60f
Dmytro <dmytro2105@gmail.com>
2024-03-27 23:13:28 +0200

    favicon fixed

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,11 +1,11 @@
-import {Suspense, useRef, useState} from 'react'
+import {Suspense, useMemo, useRef, useState} from 'react'
 import {Canvas, useFrame} from "@react-three/fiber";
 import {PointMaterial, Points, Preload} from "@react-three/drei";
 import {random} from "maath";
 
 const Stars = (props) => {
     const ref = useRef();
-    const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+    const sphere = useMemo(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }), []);
 
     useFrame((state, delta) => {
         ref.current.rotation.x -= delta / 10;
